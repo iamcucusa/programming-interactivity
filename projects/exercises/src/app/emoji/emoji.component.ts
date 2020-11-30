4f595d30de9f19562e11f8ec29a26c879c22c07b
Grace Henriquez <graciela.fernandez@businesspartner.roche.com>
2020-11-30 22:22:50 +0100

    create emoji component

--- projects/exercises/src/app/emoji/emoji.component.ts
+++ projects/exercises/src/app/emoji/emoji.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 
 @Component({
-  selector: 'app-welcome',
-  templateUrl: './emoji.component.html',
-  styleUrls: ['./emoji.component.scss']
+    selector: 'app-welcome',
+    template: `
+        <canvas #canvasEmoji width="600" height="300"></canvas>
+    `,
+    styles: ['canvas { border-style: solid }'],
+    encapsulation: ViewEncapsulation.None
 })
 export class EmojiComponent implements OnInit {
 
-  constructor() { }
+    // @ts-ignore
+    @ViewChild('canvasEmoji', {static: true}) canvasEmoji: ElementRef<HTMLCanvasElement>;
+    // @ts-ignore
+    private ctx: CanvasRenderingContext2D | null;
 
-  // tslint:disable-next-line:typedef
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+
+    ngOnInit(): void {
+        this.ctx = this.canvasEmoji.nativeElement.getContext('2d');
+    }
+
+    animate(): void {}
 
 }

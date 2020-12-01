800eb653f50c2b2b8ca95494c26e7d0419e63e2c
Grace Henriquez <graciela.fernandez@businesspartner.roche.com>
2020-12-01 22:01:12 +0100

    create a print

--- projects/exercises/src/app/class-2/my-print.component.ts
+++ projects/exercises/src/app/class-2/my-print.component.ts
@@ -3,7 +3,6 @@ import {AfterViewInit, Component, ElementRef, HostListener, ViewChild} from '@an
 @Component({
     selector: 'app-my-print-10',
     template: `
-        <button class="app-my-print-10-reload" nz-button (click)="print()">Print me again</button>
         <div>
             <canvas class="my-canvas-print-10" #myPrint width="{{canvasWidth}}" height="{{canvasHeight}}"></canvas>
         </div>
@@ -13,81 +12,55 @@ import {AfterViewInit, Component, ElementRef, HostListener, ViewChild} from '@an
 export class MyPrintComponent implements AfterViewInit {
 
     // @ts-ignore
-    @ViewChild('myPrint', {static: true}) canvasPrint10REf: ElementRef<HTMLCanvasElement>;
+    @ViewChild('myPrint', {static: true}) myPrintREf: ElementRef<HTMLCanvasElement>;
     // @ts-ignore
-    private canvasPrint10: HTMLCanvasElement | undefined;
+    private canvasMyPrint: HTMLCanvasElement | undefined;
     private context: CanvasRenderingContext2D | null | undefined;
 
     canvasWidth = 450;
     canvasHeight = 450;
 
-    columns = 15;
-    chance = 0.9;
-
     ngAfterViewInit(): void {
-        this.canvasPrint10 = this.canvasPrint10REf.nativeElement;
-        this.context = this.canvasPrint10.getContext('2d');
-        this.print();
+        this.canvasMyPrint = this.myPrintREf.nativeElement;
+        this.context = this.canvasMyPrint.getContext('2d');
+        setInterval(this.print.bind(this), 10);
 
     }
 
     sizeCanvas(): void {
         // @ts-ignore
-        this.canvasPrint10.width = this.canvasWidth;
+        this.canvasMyPrint.width = this.canvasWidth;
         // @ts-ignore
-        this.canvasPrint10.height = this.canvasHeight;
+        this.canvasMyPrint.height = this.canvasHeight;
     }
 
     @HostListener('window:resize', ['$event'])
     onResize(event: Event): void {
         this.sizeCanvas();
-        this.print();
     }
 
-    backSlash(x: number, y: number, size: number): void {
-        this.context?.beginPath();
-        this.context?.moveTo(x, y);
-        this.context?.lineTo(x + size, y + size);
-        this.context?.stroke();
-    }
-
-    forwardSlash(x: number, y: number, size: number): void {
-
-        this.context?.beginPath();
-        this.context?.moveTo(x + size, y);
-        this.context?.lineTo(x, y + size);
-        this.context?.stroke();
-    }
 
     print(): void {
 
+        // @ts-ignore
+        const x = Math.random() * this.canvasMyPrint.width;
+        // @ts-ignore
+        const y = Math.random() * this.canvasMyPrint.height;
+        const blue = Math.round(Math.random() * 255);
+        this.context?.beginPath();
+
+        // @ts-ignore random shade of pink
+        this.context.strokeStyle = `rgb(255,20,${blue})`;
 
         // @ts-ignore
-        this.context.fillStyle = 'cornflowerblue';
-        // @ts-ignore
-        this.context.fillRect(0, 0, this.canvasPrint10?.width as number, this.canvasPrint10?.height as number);
-
-        const size = this.canvasPrint10?.width as number / this.columns;
-        const rows = Math.round(this.canvasPrint10?.height as number / size);
+        this.context.lineWidth = 0.1;
 
         // @ts-ignore
-        this.context.strokeStyle = 'white';
-        // @ts-ignore
-        this.context.lineWidth = 5;
-        // @ts-ignore
-        this.context.lineCap = 'square';
-        for (let i = 0; i < this.columns; i++) {
-            for (let j = 0; j < rows; j++) {
-                const rand = Math.random();
-                if (rand > this.chance) {
-                    this.backSlash(i * size, j * size, size);
-                } else {
-                    this.forwardSlash(i * size, j * size, size);
-                }
+        this.context?.moveTo(this.canvasMyPrint.width , this.canvasMyPrint.height);
+        this.context?.lineTo(x, y);
+        this.context?.stroke();
 
-            }
 
-        }
     }
 
 }

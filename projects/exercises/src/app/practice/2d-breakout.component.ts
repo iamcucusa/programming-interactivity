e0760487a7662d2b9b9cbcf1dd33fa709d4e8dff
Grace Henriquez <graciela.fernandez@businesspartner.roche.com>
2020-12-01 18:31:46 +0100

    4th step

--- projects/exercises/src/app/practice/2d-breakout.component.ts
+++ projects/exercises/src/app/practice/2d-breakout.component.ts
@@ -30,6 +30,13 @@ export class Breakout2DComponent implements AfterViewInit {
 
     ballRadius = 10;
 
+    paddleHeight = 10;
+    paddleWidth = 75;
+    paddleX = (this.canvasWidth - this.paddleWidth) / 2;
+
+    rightPressed = false;
+    leftPressed = false;
+
     ngAfterViewInit(): void {
         this.breakoutCanvas = this.breakoutRef.nativeElement;
         this.context = this.breakoutCanvas.getContext('2d');
@@ -49,9 +56,37 @@ export class Breakout2DComponent implements AfterViewInit {
         this.sizeCanvas();
     }
 
+    @HostListener('document:keydown', ['$event'])
+    onKeyDownHandler(event: KeyboardEvent): void {
+        if (event.key === 'Right' || event.key === 'ArrowRight') {
+            this.rightPressed = true;
+        } else if (event.key === 'Left' || event.key === 'ArrowLeft') {
+            this.leftPressed = true;
+        }
+    }
+
+    @HostListener('document:keyup', ['$event'])
+    onKeyUpHandler(event: KeyboardEvent): void {
+        if (event.key === 'Right' || event.key === 'ArrowRight') {
+            this.rightPressed = false;
+        } else if (event.key === 'Left' || event.key === 'ArrowLeft') {
+            this.leftPressed = false;
+        }
+    }
+
     drawBall(): void {
         this.context?.beginPath();
         this.context?.arc(this.position.x, this.position.y, this.ballRadius, 0, Math.PI * 2);
+        // @ts-ignore
+        this.context?.fillStyle = '#0095DD';
+        this.context?.fill();
+        this.context?.closePath();
+    }
+
+    drawPaddle(): void {
+        this.context?.beginPath();
+        // @ts-ignore
+        this.context?.rect(this.paddleX, this.breakoutCanvas?.height - this.paddleHeight, this.paddleWidth, this.paddleHeight);
         // @ts-ignore
         this.context?.fillStyle = '#0095DD';
         this.context?.fill();
@@ -62,6 +97,7 @@ export class Breakout2DComponent implements AfterViewInit {
         // @ts-ignore
         this.context?.clearRect(0, 0, this.breakoutCanvas.width, this.breakoutCanvas.height);
         this.drawBall();
+        this.drawPaddle();
         this.position = {...this.position, x: this.position.x + this.dx, y: this.position.y + this.dy};
 
         // @ts-ignore
@@ -71,6 +107,20 @@ export class Breakout2DComponent implements AfterViewInit {
         // @ts-ignore
         if (this.position.y + this.dy > this.breakoutCanvas?.height - this.ballRadius || this.position.y + this.dy < this.ballRadius) {
             this.dy = -this.dy;
+        }
+
+        if (this.rightPressed) {
+            this.paddleX += 7;
+            // @ts-ignore
+            if (this.paddleX + this.paddleWidth > this.breakoutCanvas?.width) {
+                // @ts-ignore
+                this.paddleX = this.breakoutCanvas?.width - this.paddleWidth;
+            }
+        } else if (this.leftPressed) {
+            this.paddleX -= 7;
+            if (this.paddleX < 0) {
+                this.paddleX = 0;
+            }
         }
     }
 
